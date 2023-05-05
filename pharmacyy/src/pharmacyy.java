cc096dc71b66bb38084fe37072e063f2b2e1dcfc
tokanabil211 <96777822+tokanabil211@users.noreply.github.com>
2023-05-05 22:19:59 +0300

    Update pharmacyy.java

--- pharmacyy/src/pharmacyy.java
+++ pharmacyy/src/pharmacyy.java
@@ -82,50 +82,83 @@ class pharmacyy {
     }
 
     public void removeDrug() {
+        try {
+            // Open Excel file and get worksheet
+            FileInputStream file = new FileInputStream(new File("pharmacy.xlsx"));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
 
-            try {
-                // Open Excel file and get worksheet
-                FileInputStream file = new FileInputStream(new File("pharmacy.xlsx"));
-                XSSFWorkbook workbook = new XSSFWorkbook(file);
-                XSSFSheet sheet = workbook.getSheetAt(0);
+            // Get drug ID to remove from the user
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter drug ID to remove: ");
+            String idToRemove = scanner.nextLine();
+            System.out.println(" 1- remove a quantity:  " );
+            System.out.println("2- remove the entire row: ");
+            int removeOption = scanner.nextInt();
 
-                // Get drug ID to remove from the user
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Enter drug ID to remove: ");
-                String idToRemove = scanner.nextLine();
+            // Search for drug by ID
+            int maxRows = sheet.getLastRowNum();
+            boolean drugFound = false;
+            int rowIndexToRemove = -1;
+            for (int i = 1; i <= maxRows; i++) {
+                Row row = sheet.getRow(i);
+                if (row.getCell(1).getCellTypeEnum() == CellType.STRING && row.getCell(1).getStringCellValue().equals(idToRemove)) {
+                    drugFound = true;
+                    rowIndexToRemove = i;
+                    break;
+                }
+            }
 
-                // Search for drug by ID
-                int maxRows = sheet.getLastRowNum();
-                boolean drugFound = false;
-                for (int i = 1; i <= maxRows; i++) {
-                    Row row = sheet.getRow(i);
-                    if (row.getCell(1).getCellTypeEnum() == CellType.STRING && row.getCell(1).getStringCellValue().equals(idToRemove)) {
-                        //sheet.shiftRows(i + 1, maxRows - 1, -1); // Shift cells upward
-                        sheet.removeRow(row);
-                        sheet.shiftRows(i + 1, maxRows, -1); // Shift cells upward
-                         // Remove the row
-
-                        drugFound = true;
-                        System.out.println("Drug removed from the pharmacy successfully.");
-                        break;
+            // Remove drug if found
+            // Remove drug if found
+            // Remove drug if found
+            if (drugFound) {
+                if (rowIndexToRemove == -1) {
+                    System.out.println("Unexpected error occurred while trying to remove the drug.");
+                } else {
+                    Row rowToRemove = sheet.getRow(rowIndexToRemove);
+                    if (rowToRemove == null) {
+                        System.out.println("Unexpected error occurred while trying to remove the drug.");
+                    } else if (removeOption == 1) {
+                        System.out.print("Enter quantity to remove: ");
+                        int quantityToRemove = scanner.nextInt();
+                        int currentQuantity = (int) rowToRemove.getCell(4).getNumericCellValue();
+                        int newQuantity = currentQuantity - quantityToRemove;
+                        if (newQuantity < 0) {
+                            System.out.println("Error: Quantity to remove is greater than current quantity.");
+                        } else if (newQuantity == 0) {
+                            sheet.removeRow(rowToRemove);
+                            System.out.println("Drug removed from pharmacy successfully.");
+                        } else {
+                            rowToRemove.getCell(4).setCellValue(newQuantity);
+                            System.out.println("Drug quantity updated successfully.");
+                        }
+                    } else if (removeOption == 2) {
+                        sheet.removeRow(rowToRemove);
+                        int lastRow = sheet.getLastRowNum();
+                        if (rowIndexToRemove >= 0 && rowIndexToRemove < lastRow) {
+                            sheet.shiftRows(rowIndexToRemove + 1, lastRow, -1);
+                        }
+                        System.out.println("Drug removed from pharmacy successfully.");
+                    } else {
+                        System.out.println("Invalid option selected.");
                     }
                 }
-
-                // Handle drug not found case
-                if (!drugFound) {
-                    System.out.println("Drug not found in the pharmacy.");
-                }
-
-                // Save changes to the Excel file
-                FileOutputStream fileOut = new FileOutputStream(new File("pharmacy.xlsx"));
-                workbook.write(fileOut);
-                fileOut.close();
-
-                // Close Excel file
-                workbook.close();
-                file.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                System.out.println("Drug with ID " + idToRemove + " not found in pharmacy.");
             }
+
+
+            // Save changes to Excel file
+            FileOutputStream fileOut = new FileOutputStream(new File("pharmacy.xlsx"));
+            workbook.write(fileOut);
+            fileOut.close();
+
+            // Close Excel file
+            workbook.close();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+}
