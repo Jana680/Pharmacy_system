8680eca0d666158ada4738547b037555cfbf009f
tokanabil211 <96777822+tokanabil211@users.noreply.github.com>
2023-05-11 01:19:43 +0300

    Update pharmacyy.java

--- pharmacyy/src/pharmacyy.java
+++ pharmacyy/src/pharmacyy.java
@@ -7,16 +7,21 @@ import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.Scanner;
 
 class pharmacyy {
 
     private static final int MAX_DRUGS = 20;
+    //private HashMap<K, Integer> drugQuantityMap;
 
-    public void addDrug(String type1) {
+    public void addDrug(String type1) throws InputMismatchException {
         try {
             // open Excel file and get worksheet
-            FileInputStream file = new FileInputStream(new File("pharmacy.xlsx"));
+            FileInputStream file = new FileInputStream(new File("pharmacyy.xlsx"));
             XSSFWorkbook workbook = new XSSFWorkbook(file);
             XSSFSheet sheet = workbook.getSheetAt(0);
 
@@ -69,7 +74,7 @@ class pharmacyy {
             }
 
             // save changes to Excel file
-            FileOutputStream fileOut = new FileOutputStream(new File("pharmacy.xlsx"));
+            FileOutputStream fileOut = new FileOutputStream(new File("pharmacyy.xlsx"));
             workbook.write(fileOut);
             fileOut.close();
 
@@ -84,15 +89,15 @@ class pharmacyy {
     public void removeDrug() {
         try {
             // Open Excel file and get worksheet
-            FileInputStream file = new FileInputStream(new File("pharmacy.xlsx"));
+            FileInputStream file = new FileInputStream(new File("pharmacyy.xlsx"));
             XSSFWorkbook workbook = new XSSFWorkbook(file);
             XSSFSheet sheet = workbook.getSheetAt(0);
 
-            // Get drug ID to remove from the user
+            // Get drug ID to remove
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter drug ID to remove: ");
             String idToRemove = scanner.nextLine();
-            System.out.println(" 1- remove a quantity:  " );
+            System.out.println(" 1- remove a quantity:  ");
             System.out.println("2- remove the entire row: ");
             int removeOption = scanner.nextInt();
 
@@ -109,9 +114,7 @@ class pharmacyy {
                 }
             }
 
-            // Remove drug if found
-            // Remove drug if found
-            // Remove drug if found
+
             if (drugFound) {
                 if (rowIndexToRemove == -1) {
                     System.out.println("Unexpected error occurred while trying to remove the drug.");
@@ -150,7 +153,7 @@ class pharmacyy {
 
 
             // Save changes to Excel file
-            FileOutputStream fileOut = new FileOutputStream(new File("pharmacy.xlsx"));
+            FileOutputStream fileOut = new FileOutputStream(new File("pharmacyy.xlsx"));
             workbook.write(fileOut);
             fileOut.close();
 
@@ -159,6 +162,125 @@ class pharmacyy {
             file.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    public void placeAnOrder() throws InputMismatchException {
+        try {
+            FileInputStream file = new FileInputStream(new File("pharmacyy.xlsx"));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter drug ID to check availability: ");
+            String idToCheck = scanner.nextLine().trim();
+
+            boolean drugFound = false;
+            double totalSales = 0.0;
+            int totalQuantity = 0;
+            int newQuantity =0;
+            Map<String, Integer> drugQuantityMap = new HashMap<>();
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+                Cell idCell = row.getCell(1);
+                String cellValue = "";
+
+                CellType idCellType = idCell.getCellTypeEnum();
+                if (idCellType == CellType.STRING) {
+                    cellValue = idCell.getStringCellValue().trim();
+                } else if (idCellType == CellType.NUMERIC) {
+                    cellValue = String.valueOf((int) idCell.getNumericCellValue());
+                }
+
+                if (idCellType == CellType.STRING && cellValue.equalsIgnoreCase(idToCheck)) {
+                    drugFound = true;
+                    Cell nameCell = row.getCell(0);
+                    Cell unitPriceCell = row.getCell(2);
+                    Cell categoryCell = row.getCell(3);
+                    Cell quantityCell = row.getCell(4);
+
+                    String drugName = nameCell.getStringCellValue();
+                    double unitPrice = 0.0;
+                    double quantity = quantityCell.getNumericCellValue();
+
+                    CellType unitPriceCellType = unitPriceCell.getCellTypeEnum();
+                    if (unitPriceCellType == CellType.NUMERIC) {
+                        unitPrice = unitPriceCell.getNumericCellValue();
+                    } else if (unitPriceCellType == CellType.STRING) {
+                        try {
+                            unitPrice = Double.parseDouble(unitPriceCell.getStringCellValue());
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid unit price for drug with ID " + idToCheck);
+                            break;
+                        }
+                    }
+
+                    String category = categoryCell.getStringCellValue();
+
+                    if (category.equals("cosmetics")) {
+                        unitPrice *= 1.2;
+                    }
+
+                    System.out.println("Drug: " + drugName);
+                    System.out.println("Unit Price: " + unitPrice);
+
+                    if (category.equals("prescription")) {
+                        System.out.print("Prescription required. Do you have a prescription? (yes/no): ");
+                        String prescriptionResponse = scanner.nextLine().trim().toLowerCase();
+                        if (prescriptionResponse.equals("no")) {
+                            System.out.println("Sorry, you need a prescription to purchase this drug.");
+                            break;
+                        }
+                    }
+
+                    System.out.print("Enter quantity: ");
+                    int orderedQuantity = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline character
+
+                    if (orderedQuantity > quantity) {
+                        System.out.println("Insufficient quantity available.");
+                        break;
+                    }
+
+                    quantity -= orderedQuantity;
+                    quantityCell.setCellValue(quantity);
+
+//                    System.out.print("Enter quantity: ");
+//                    int quantity = scanner.nextInt();
+//                    scanner.nextLine(); // Consume the newline character
+
+                    // Update the quantity for the drug
+                   // Map<K, Integer> drugQuantityMap;
+                    int previousQuantity = drugQuantityMap.getOrDefault(idToCheck, 0);
+                     newQuantity = (int) (previousQuantity + quantity);
+                    drugQuantityMap.put(idToCheck, newQuantity);
+
+                    double totalPrice = unitPrice * orderedQuantity;
+                    totalSales += totalPrice;
+
+                    System.out.println("Total Price: " + totalPrice);
+                    System.out.println("Order placed successfully.");
+                    break;
+                }
+            }
+
+            if (!drugFound) {
+                System.out.println("Drug with ID " + idToCheck + " not found in pharmacy.");
+            }
+
+           // System.out.println("Total Quantity for the day: " + newQuantity);
+            System.out.println("Total Sales for the day: " + totalSales);
+
+            FileOutputStream outFile = new FileOutputStream(new File("pharmacyy.xlsx"));
+            workbook.write(outFile);
+            outFile.close();
+
+            workbook.close();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
         }
     }
 }
