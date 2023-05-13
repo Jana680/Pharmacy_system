c48b6638922f4605345173011714625204aff473
Jana <102023114+JanaAmerMohamed@users.noreply.github.com>
2023-05-13 05:06:43 +0300

    files
    GUI for the project

---
+++ pharmacygui/src/main/java/com/mycompany/pharmacygui/Pharmacygui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pharmacygui;
+
+/**
+ *
+ * @author jana2
+ */
+public class Pharmacygui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
